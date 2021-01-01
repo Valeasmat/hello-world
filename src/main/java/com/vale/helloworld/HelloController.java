7b6039150249d193a1b04cb195cbd45c59354597
Valeasmat <valeriaasmat2dob@gmail.com>
2021-01-01 18:07:46 -0500

    childish error

--- src/main/java/com/vale/helloworld/HelloController.java
+++ src/main/java/com/vale/helloworld/HelloController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @GetMapping
+    @GetMapping("/{name}")
     public String greeting(@PathVariable String name){
         return "Hi "+name+", welcome from kubectl";
     }
